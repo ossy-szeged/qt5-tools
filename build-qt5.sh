71196b619189d4f8202e019e5843b27b1b2e061e
Csaba Osztrogonác <oszi@inf.u-szeged.hu>
2012-02-03 16:50:50 +0100

    weekly update

--- build-qt5.sh
+++ build-qt5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-NEW_QTDIR=/usr/local/Trolltech/Qt5/Qt-5.0.0-r20
-WEEKLY_QT5_HASH="cb9558f041132ea2c6c0acb360214c5bab4376bf"
+NEW_QTDIR=/usr/local/Trolltech/Qt5/Qt-5.0.0-r21
+WEEKLY_QT5_HASH="48a84504374730c6eb14c265f2fe96a4cfe02294"
 THREADS=-j30
 QT5_MODULES="qtjsbackend qtxmlpatterns qtscript qtdeclarative qt3d qtsensors qtlocation"
 
@@ -9,13 +9,14 @@ cd qt5
 git checkout master
 git clean -dxf
 git reset --hard HEAD
-git submodule foreach "git reset --hard HEAD"
+git submodule foreach "git checkout master"
 git submodule foreach "git clean -dxf"
-git fetch
-git checkout $WEEKLY_QT5_HASH
+git submodule foreach "git reset --hard HEAD"
+git fetch || exit 1
+git reset --hard $WEEKLY_QT5_HASH || exit 1
 ./init-repository --module-subset=qtbase,`echo $QT5_MODULES | tr " " ","` -f || exit 1
-git submodule foreach git fetch
-git submodule update --recursive
+git submodule foreach "git fetch" || exit 1
+git submodule update --recursive || exit 1
 echo ==========================================================
 git submodule status
 echo ==========================================================
@@ -23,7 +24,7 @@ echo ==========================================================
 export QTDIR=$NEW_QTDIR
 export PATH=$QTDIR/bin:$PATH
 
-./configure -opensource -confirm-license -no-pch -nomake examples -nomake demos -nomake tests -no-webkit -no-gtkstyle -nomake translations -qt-zlib -qt-libtiff -qt-libpng -qt-libjpeg -qt-sql-sqlite -release -prefix $QTDIR
+./configure -opensource -confirm-license -no-pch -nomake examples -nomake demos -nomake tests -no-gtkstyle -nomake translations -qt-zlib -qt-libpng -qt-libjpeg -qt-sql-sqlite -release -prefix $QTDIR
 
 cd qtbase && make $THREADS && make install && cd ..
 if [ $? -ne 0 ] ; then
